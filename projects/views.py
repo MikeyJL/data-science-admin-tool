cff18f3999ee361f2e1fed8f0314a1058c6000f8
Mikey Lau <63963445+MikeyJL@users.noreply.github.com>
2022-10-28 19:46:08 +0100

    feat: Add frontend and bundler

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse, HttpRequest
-from django.template import loader
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    template = loader.get_template("projects/index.html")
-    return HttpResponse(template.render())
+    return HttpResponse("Success")
