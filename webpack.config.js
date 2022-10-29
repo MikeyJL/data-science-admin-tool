3b05b72f88d228ef9662c44b60085255f2a79f46
Mikey Lau <63963445+MikeyJL@users.noreply.github.com>
2022-10-29 10:58:04 +0100

    feat: Add frontend and bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./frontend/index.js",
+	entry: "./frontend/index.tsx",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
 	output: {
-		filename: "index-bundle.js",
+		filename: "bundle.js",
 		path: path.resolve(__dirname, "./static"),
 	},
 };
