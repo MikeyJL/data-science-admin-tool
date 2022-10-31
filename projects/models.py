c472bf7a0e17a511af0e803fd499e4ea63a4f25e
Mikey Lau <63963445+MikeyJL@users.noreply.github.com>
2022-10-31 20:36:56 +0000

    refactor: Update projects model

--- projects/models.py
+++ projects/models.py
@@ -3,3 +3,7 @@ from django.db.models import Model, CharField
 
 class Project(Model):
     name = CharField(max_length=100)
+    description = CharField(max_length=500, null=True)
+
+    def __str__(self) -> str:
+        return self.name
