ec5d190e5be3bf79b2a5072a359ee14af09453a7
Mikey Lau <63963445+MikeyJL@users.noreply.github.com>
2022-11-09 14:13:30 +0000

    fix: Remove password from change form

--- cognito/admin.py
+++ cognito/admin.py
@@ -75,7 +75,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ("email", "is_admin")
     list_filter = ("is_admin",)
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
+        (None, {"fields": ("email",)}),
         ("Permissions", {"fields": ("is_admin",)}),
     )
     add_fieldsets = (
