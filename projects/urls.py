9d4dd5f921e9575e992d8862acd9e5890bf48c17
Mikey Lau <63963445+MikeyJL@users.noreply.github.com>
2022-11-22 16:02:26 +0000

    feat: New endpoint to retrieve one project

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from .views import ProjectsView
 
 urlpatterns = [
-    path("", ProjectsView.as_view()),
+    path("", ProjectsView.as_view({"get": "get_all"})),
+    path("<uuid:project_id>", ProjectsView.as_view({"get": "get_one"})),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,31 +1,49 @@
 """Views for Project app."""
 
+from uuid import UUID
+
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
-from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 
 from .models import Project
 from .serializers import ProjectSerializer
 
 
-class ProjectsView(APIView):
+class ProjectsView(ViewSet):
     """Project view."""
 
     authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request: Request) -> Response:
-        """Get all the projects.
+    def get_all(self, request: Request) -> Response:
+        """Get all projects.
 
         Args:
-            request (Request): the incoming request.
+            request (Request): request
 
         Returns:
-            Response: the list of projects available.
+            Response: all the projects.
         """
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
+
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def get_one(self, request: Request, project_id: UUID | str) -> Response:
+        """Get a single project.
+
+        Args:
+            request (Request): request
+            project_id (str | None): project_id
+
+        Returns:
+            Response: the project details.
+        """
+        project = Project.objects.get(id=project_id)
+        serializer = ProjectSerializer(project)
+
         return Response(serializer.data, status=HTTP_200_OK)
